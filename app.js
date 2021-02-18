c7e3c4e168cef7d935b9550a0d423c6158f01ae1
Jerzy Pędlowski <jerzy.pedlowski@gmail.com>
2021-02-18 22:11:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+//import routes
+const homeRoute = require("./routes/home");
+const postRoute = require("./routes/posts");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//Routes
+app.use("/posts", postRoute);
+app.use("/", homeRoute);
+
+app.listen(3000);
+
+//Connecting to database
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true,useUnifiedTopology: true });
